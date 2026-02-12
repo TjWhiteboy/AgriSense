f4bed162553171fb2dc2d34bb9c1b16170dca7c8
Thikash Jayaprakash <thikash772006@gmail.com>
2026-02-12 21:43:55 +0530

    Delete AgriSense-Main/AgriSense-Main directory

--- AgriSense-Main/AgriSense-Main/components/icons/SendIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SendIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-    <path d="M3.105 2.289a.75.75 0 00-.826.946l2.422 7.925a.75.75 0 00.726.54h6.352a.75.75 0 010 1.5H5.427l-2.422 7.925a.75.75 0 00.826.946 60.317 60.317 0 0015.468-8.25.75.75 0 000-1.228A60.317 60.317 0 003.105 2.289z" />
-  </svg>
-);
-
-export default SendIcon;
